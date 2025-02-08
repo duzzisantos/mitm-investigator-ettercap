68e74ec9213b28b22a8fe95c951723b328c041b7
Duzie <duzzi4x@gmail.com>
2025-02-08 18:30:23 +0100

    Added request body data model

--- routers/EtterCapProcess.py
+++ routers/EtterCapProcess.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 from api.RunEttercap import run_ettercap
 
 ettercap_process = APIRouter()
 
 
+class NetworkInterface(BaseModel):
+    interface: str
+
+
 @ettercap_process.post("/GetEtterCapInvestigation")
-def use_ettercap():
-    return run_ettercap("en0")
+def use_ettercap(item: NetworkInterface):
+    return run_ettercap(item.interface)
